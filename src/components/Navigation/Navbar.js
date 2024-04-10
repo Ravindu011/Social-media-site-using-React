f8a3de191ef4ad5a75033e6e9a3a40094b54f2bb
Ravindu C Rathnayake <ravinduc2001@gmail.com>
2024-04-10 15:47:14 +0530

    Navigation bar created

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import "./navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className='navBarBox'>
+        <div className="navBarLeft">
+            <span className="logo">marketbook</span>
+        </div>
+        <div className="navBarCenter">
+            <div className="searchBox">
+                <SearchIcon className='searchIcon'/>
+                <input placeholder='Search' className="searchInput" />
+            </div>
+        </div>
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                <span className="navBarLinks">HomePage</span>
+                <span className="navBarLinks">Profile</span>
+            </div>
+            <div className="navBarIcons">
+                <div className="navBarIcon">
+                    <PersonIcon/>
+                    <div className="iconTag">3</div>
+                </div>
+                <div className="navBarIcon">
+                    <ChatBubbleIcon/>
+                    <span className="iconTag">5</span>
+                </div>
+                <div className="navBarIcon">
+                    <SettingsSuggestIcon/>
+                    <span className="iconTag">2</span>
+                </div>
+                <div className="profilePic">
+                    <img src="/images/1.jpg" alt="" className="prPic" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
